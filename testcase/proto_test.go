f86875d39bfdbaac2d2aaa4d8a77cd568f23c6fa
ji.wu <ji.wu@nio.com>
2024-10-12 18:30:36 +0800

    repair

--- testcase/proto_test.go
+++ testcase/proto_test.go
@@ -2,6 +2,7 @@ package testcase
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/smartystreets/goconvey/convey"
 	"github.com/ucanme/proto_parser/parser"
 	"reflect"
@@ -10,18 +11,55 @@ import (
 
 func TestMarshal2Json(t *testing.T) {
 	convey.Convey("TestMarshal2Json", t, func() {
-		msgStr := ``
+		msgStr :=
+			`enum State {
+    STATE_ONE= 0;
+    STATE_TWO   = 1;
+    STATE_THREE = 2;
+    STATE_FOUR    = 3;
+    STATE_FIVE   = 7;
+}
+
+enum LiveState {
+    LIVE_ST_NONE=0;
+    LIVE_ST_MD=1;
+    LIVE_ST_AD= 2;
+    LIVE_ST_INVALID=7;
+}
+
+enum StateDetailed {
+    ST_PARKED   = 0;
+    ST_DRVRDY   = 1;
+    ST_DRIVING  = 2;
+    ST_SWUPDATE = 3;
+    ST_CHARGING = 4;
+    ST_PWRSWAP  = 5;
+    ST_INVALID = 15;
+}
+enum STStateDetailed {
+    ST_DETAILED_NONE    = 0;
+    ST_DETAILED_MD      = 1;
+    ST_DETAILED_NAD     = 2;
+    ST_DETAILED_RD      = 3;
+    ST_DETAILED_INVALID = 7;
+}
+message StateMsg {
+    StateDetailed state_detailed = 1;
+    State state                  = 2;
+    STStateDetailed st_state_detailed = 3;
+    LiveState live_state = 4;
+}`
+
 		p, err := parser.Parse([]byte(msgStr))
+		fmt.Println("---p----", p)
 		convey.So(err, convey.ShouldBeNil)
-		m, err := p.Unmarshal2Json("SomeMessageName", []byte{0x0a, 0x02, 0x08, 0x01, 0x0a, 0x02, 0x08, 0x01, 0x0a, 0x02, 0x08, 0x01, 0x0a, 0x02, 0x08, 0x01, 0x10, 0x02, 0x1a, 0x0c, 0x28, 0x01, 0x30, 0x02, 0x38, 0x04, 0x40, 0x05, 0x48, 0x03, 0x50, 0x01, 0x1a, 0x0c, 0x28, 0x08, 0x30, 0x09, 0x38, 0x0b, 0x40, 0x0c, 0x48, 0x0a, 0x50, 0x01, 0x1a, 0x0c, 0x28, 0x0f, 0x30, 0x10, 0x38, 0x12, 0x40, 0x13, 0x48, 0x11, 0x50, 0x01, 0x1a, 0x0c, 0x28, 0x16, 0x30, 0x17, 0x38, 0x19, 0x40, 0x1a, 0x48, 0x18, 0x50, 0x01, 0x22, 0x04, 0x06, 0x0d, 0x14, 0x1b, 0x2a, 0x04, 0x01, 0x02, 0x03, 0x04, 0x32, 0x04, 0x07, 0x0e, 0x15, 0x1c})
+		m, err := p.Unmarshal2Json("StateMsg", []byte{0x08, 0x03, 0x10, 0x02, 0x18, 0x03})
 		convey.So(err, convey.ShouldBeNil)
-		data, err := json.Marshal(m)
-		convey.So(err, convey.ShouldBeNil)
-		expectJsonStr := `{`
-		ok, err := DeepEqualJson(string(data), expectJsonStr)
+		data, _ := json.Marshal(m)
+
+		ok, err := DeepEqualJson(string(data), `{"live_state":0,"st_state_detailed":3,"state":2,"state_detailed":3}`)
 		convey.So(err, convey.ShouldBeNil)
 		convey.So(ok, convey.ShouldBeTrue)
-
 	})
 }
 
